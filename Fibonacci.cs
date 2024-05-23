82c298a89e09a746fcd3c819252bbfd93dec9c4a
Ariel Alvarado <arielalvarado15@protonmail.com>
2024-05-23 15:19:49 -0600

    elaboracion de logica de la secuencia fibonacci

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -11,7 +11,25 @@ namespace cleancode
         public static void main()
         {
             Console.WriteLine("\tSerie de Fibonacci");
-            Console.WriteLine("Ingrese el número de la serie de Fibonacci que desea mostrar:");
+            Console.WriteLine("Ingrese la cantidad de terminos de la secuencia Fibonacci que desea mostrar:");
+            int limiteSecuenciaFibonacci = int.Parse(Console.ReadLine());
+            int primerNumero = 0;
+            int segundoNumero = 1;
+            int siguienteNumero = 0;
+            for (int i = 0; i < limiteSecuenciaFibonacci; i++)
+            {
+                if(i <= 1)
+                {
+                    siguienteNumero = i;
+                }
+                else
+                {
+                    siguienteNumero = primerNumero + segundoNumero;
+                    primerNumero = segundoNumero;
+                    segundoNumero = siguienteNumero;
+                }
+                Console.Write(siguienteNumero + " ");
+            }
            
         }
     }
