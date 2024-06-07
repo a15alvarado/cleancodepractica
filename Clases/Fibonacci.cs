9b64e2cfadc92c12332a75a7d0bac228f6cf631c
Ariel Alvarado <arielalvarado15@protonmail.com>
2024-06-07 12:04:34 -0600

    refactorzado

--- Clases/Fibonacci.cs
+++ Clases/Fibonacci.cs
@@ -8,31 +8,27 @@ namespace CleanCode.Clases
 {
     internal class Fibonacci
     {
+        public static int limiteSecuenciaFibonacci = 0;
+        public static int primerNumero = 0;
+        public static int segundoNumero = 1;
+        public static int siguienteNumero = 0;
         public static void ObtenerSecuenciaFibonacci()
         {
             Console.WriteLine("\tSerie de Fibonacci");
             Console.WriteLine("Ingrese la cantidad de terminos de la secuencia Fibonacci que desea mostrar:");
-            int limiteSecuenciaFibonacci = int.Parse(Console.ReadLine());
-            int primerNumero = 0;
-            int segundoNumero = 1;
-            int siguienteNumero = 0;
-            Console.Write("Cadena de Fibonacci de los primeros " + limiteSecuenciaFibonacci + " términos: ");
+            limiteSecuenciaFibonacci = int.Parse(Console.ReadLine());
+            Console.Write("Cadena de Fibonacci de los primeros " + limiteSecuenciaFibonacci + " términos: ");GenerarSecuenciaFibonacci();
+        }
+        public static void GenerarSecuenciaFibonacci()
+        {
             for (int i = 0; i < limiteSecuenciaFibonacci; i++)
             {
-                if (i <= 1)
-                {
-                    siguienteNumero = i;
-                }
-                else
-                {
-                    siguienteNumero = primerNumero + segundoNumero;
-                    primerNumero = segundoNumero;
-                    segundoNumero = siguienteNumero;
-                }
                 Console.Write(siguienteNumero + " ");
+                primerNumero = segundoNumero;
+                segundoNumero = siguienteNumero;
+                siguienteNumero = primerNumero + segundoNumero;
             }
             Console.WriteLine();
-
         }
     }
 }
